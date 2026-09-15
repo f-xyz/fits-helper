d7d37c79fb23622b9618c075a5d52fbde9196943
fxyz <andrey.lischina@gmail.com>
2026-09-15 12:21:22 +0300

    Added utils::string::quote(s)

---
+++ astroutils/tests/string/string-misc-tests.cpp
@@ -0,0 +1,10 @@
+#include "string.hpp"
+
+TEST(StringMisc, quote) {
+  // arrange
+  std::string str = "hello";
+  // act
+  auto result = utils::string::quote(str);
+  // assert
+  EXPECT_EQ(result, "\"hello\"");
+}
